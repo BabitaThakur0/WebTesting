53bde2c9c635f594d0f8874d40041f9922ce87e5
Babita thakur <babitathakur@Babitas-MacBook-Air.local>
2023-02-04 16:18:07 +0530

    webTesting

---
+++ amazon/src/main/java/Webtesting/App.java
@@ -0,0 +1,13 @@
+package Webtesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
